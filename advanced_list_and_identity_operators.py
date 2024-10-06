b39accd347a74ffc19942524273a307d8e9e0032
bryanpereira <gfxmagicz@gmail.com>
2024-10-06 00:50:37 -0400

    Advanced Lists and Identity Operators completed

---
+++ advanced_list_and_identity_operators.py
@@ -0,0 +1,6 @@
+#task1
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+if "Alice" in submitted and attended: print("Alice submitted her assignment and was in class")
+
