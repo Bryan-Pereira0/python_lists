8d8ab3f2c2975b454c1e9c82b90d296dec5c054e
haru <gfxmagicz@gmail.com>
2024-10-06 20:02:31 -0400

    Update advanced_list_and_identity_operators.py
    fixed identity error

--- advanced_list_and_identity_operators.py
+++ advanced_list_and_identity_operators.py
@@ -2,5 +2,6 @@
 submitted = ["Alice", "Bob", "Charlie", "David"]
 attended = ["Charlie", "Eve", "Alice", "Frank"]
 
-if "Alice" in submitted and attended: print("Alice submitted her assignment and was in class")
+if "Alice" in submitted and "Alice" in attended:
+  print("Alice submitted her assignment and was in class")
 
